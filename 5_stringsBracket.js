3644d4ac74101476d3823d85e7f2340a501802ff
Prasanna Kulkarni <104126691+EyesofHeaven12@users.noreply.github.com>
2023-01-27 22:46:02 +0530

    Add files via upload

---
+++ 5_stringsBracket.js
@@ -0,0 +1,9 @@
+/* String! [Bracket Notation]*/
+var firstName = "Thomas"
+
+// first charecter 
+console.log(firstName[1])
+
+// last charecter
+console.log(firstName[firstName.length - 1])
+// String values are imutable (can not changed)
